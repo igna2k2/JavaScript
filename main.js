7d84f4a8f1c20bf2b4430f9d12fa4c7233f2c1e9
igna2k2 <ignabaci@gmail.com>
2023-08-03 22:19:08 -0300

    commit

--- main.js
+++ main.js
@@ -1,36 +1,63 @@
-// Simulador interactivo
-function simuladorInteractivo() {
-    console.log("Simulador de Conversión de Yuan");
-    let yuan = parseFloat(prompt("Ingresa la cantidad en yuan que deseas convertir:"));
-  
-    // Conversión a dólares
-    const tipoCambioDolar = 0.14;
-    let dolares = yuan * tipoCambioDolar
-  
-    // Determinar el mensaje de conversión
-    let mensajeDolares = "";
-    if (yuan === 1) {
-      mensajeDolares = yuan + " yuan equivale a " + dolares + " dólares.";
-    } else {
-      mensajeDolares = yuan + " yuanes equivalen a " + dolares + " dólares.";
-    }
-  
-    console.log(mensajeDolares);
-  
-    // Conversión a pesos
-    const tipoCambioPeso = 40;
-    let pesos = yuan * tipoCambioPeso;
-  
-    // Determinar el mensaje de conversión
-    let mensajePesos = "";
-    if (yuan === 1) {
-      mensajePesos = yuan + " yuan equivale a " + pesos + " pesos.";
-    } else {
-      mensajePesos = yuan + " yuanes equivalen a " + pesos + " pesos.";
-    }
-  
-    console.log(mensajePesos);
+// Conversión a dólares
+const tipoCambioDolar = 0.14;
+
+const conversionesIndividuales = [];
+
+function agregarConversion() {
+  const yuanInput = document.getElementById("yuanInput");
+  const yuan = parseFloat(yuanInput.value);
+
+  if (isNaN(yuan)) {
+    alert("Debes ingresar una cantidad válida en yuan para realizar la conversión.");
+    return;
   }
-  
-  // Ejecución del simulador interactivo
-  simuladorInteractivo();
+
+  yuanInput.value = "";
+
+  const dolares = yuan * tipoCambioDolar;
+
+  let mensajeDolares = "";
+  if (yuan === 1) {
+    mensajeDolares = yuan + " yuan equivale a " + dolares.toFixed(2) + " dólares.";
+  } else {
+    mensajeDolares = yuan + " yuanes equivalen a " + dolares.toFixed(2) + " dólares.";
+  }
+
+  conversionesIndividuales.push(dolares);
+
+  const conversionIndividual = document.createElement("div");
+  conversionIndividual.textContent = mensajeDolares;
+
+  const resultadoIndividualSection = document.getElementById("resultadoDolares");
+  resultadoIndividualSection.appendChild(conversionIndividual);
+}
+
+function calcularTotal() {
+  if (conversionesIndividuales.length === 0) {
+    alert("Debes agregar al menos una conversión antes de calcular el total.");
+    return;
+  }
+
+  let totalDolares = 0;
+  conversionesIndividuales.forEach((dolares) => {
+    totalDolares += dolares;
+  });
+
+  const totalDolaresSection = document.getElementById("totalDolares");
+  totalDolaresSection.textContent = `Inventario total en dólares: ${totalDolares.toFixed(2)} dólares.`;
+}
+
+function borrarNumero() {
+  const yuanInput = document.getElementById("yuanInput");
+  yuanInput.value = ""; 
+}
+
+function borrarConversiones() {
+  const resultadoIndividualSection = document.getElementById("resultadoDolares");
+  resultadoIndividualSection.innerHTML = "";
+
+  const totalDolaresSection = document.getElementById("totalDolares");
+  totalDolaresSection.textContent = "Inventario total en dólares: 0.00 dólares.";
+
+  conversionesIndividuales.length = 0;
+}
